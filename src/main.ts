77b658946b87782a10d9ca9d419a7b29f01d7169
Bradley Nelson <bradley@nel.family>
2023-02-18 23:31:13 -0700

    addons

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,23 @@
 import { Terminal } from "xterm"
 import { TerminalManager } from "./terminalManager"
+import { FitAddon } from 'xterm-addon-fit';
+import { WebLinksAddon } from 'xterm-addon-web-links';
 
 const terminalContainer = document.querySelector<HTMLDivElement>('#terminal')
 
 if (terminalContainer) {
+  const fitAddon = new FitAddon();
   const terminal = new Terminal({
     cursorBlink: true,
     convertEol: true,
     scrollback: 1000,
     fontFamily: 'Roboto Mono, Ubuntu Mono, courier-new, courier, monospace',
   })
-  terminal.open(terminalContainer)
+  terminal.loadAddon(fitAddon);
+  terminal.loadAddon(new WebLinksAddon());
+  terminal.open(terminalContainer);
+  fitAddon.fit();
+  addEventListener('resize', () => fitAddon.fit());
   new TerminalManager(terminal);
 } else {
   console.error('Terminal container not found')
